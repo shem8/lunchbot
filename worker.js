aaf32403830a06d895b14c805ebbb401f2a3f9ce
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-09-08 16:23:33 +0300

    msg

--- worker.js
+++ worker.js
@@ -1,47 +1,59 @@
-var Queue = require('bull');
+const Queue = require('bull');
 const https = require('https');
+const lib = require('lib');
 
 var queue = new Queue('lunch', process.env.REDIS_URL);
 
 console.log('setup');
 queue.process(function(job, done){
-  // transcode audio asynchronously and report progress
-//  job.progress(42);
-
-  // call done when finished
-//  done();
-
-  // or give a error if error
-//  done(new Error('error transcoding'));
-
-  // or pass it a result
-//  done(null, { samplerate: 48000 /* etc... */ });
-
-  // If the job throws an unhandled exception it is also handled correctly
-//  throw new Error('some unexpected error');
-  console.log('test');
   console.log(job.data);
   const {
     msg,
     channel,
     team,
+    type,
   } = job.data;
 
-  const url = `https://shem.lib.id/lunchtime@dev/webhook/channel/?channel=${channel}&team_id=${team}`;
-  console.log(url);
+  // jsonObject = JSON.stringify({
+  //   type : type,
+  // });
+  //
+  // // prepare the header
+  // var postheaders = {
+  //     'Content-Type' : 'application/json',
+  //     'Content-Length' : Buffer.byteLength(jsonObject, 'utf8')
+  // };
+  //
+  // // the post options
+  // var optionspost = {
+  //     host : 'shem.lib.id',
+  //     port : 443,
+  //     path : `/lunchtime@dev/webhook/channel/?channel=${channel}&team_id=${team}`,
+  //     method : 'POST',
+  //     headers : postheaders
+  // };
+  //
+  // https.request(optionspost, (res) => {
+  //   console.log('STATUS: ' + res.statusCode);
+  //   console.log('HEADERS: ' + JSON.stringify(res.headers));
+  //   res.setEncoding('utf8');
+  //   res.on('data', function (chunk) {
+  //     console.log('BODY: ' + chunk);
+  //   });
+  //   done();
+  // }).end();
 
-  https.get(url, (res) => {
-    console.log('STATUS: ' + res.statusCode);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-    });
-    done();
-  }).end();
-
-console.log('end');
-
+  lib.shem.webhook({
+    channel: channel,
+    team_id: team,
+    type: type,
+  }, function (err, result) {
+    if (err) {
+      console.log('err: ' + err);
+      return;
+    }
+    console.log('result: ' + result);
+  });
 });
 
 queue.on('error', function(error) {
