18d0e555d10ce2266fa8c581815e0ba86c7eb3d3
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-09-20 16:16:57 +0300

    fix database connection

--- models.js
+++ models.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
-const sequelize = new Sequelize({
-        "username": "smbjtrlrzpewae",
-        "password": "1e5a83e317065456cb66e3bcbc6fd46c8243f79f7bb09eb990ec844578746734",
-        "database": "d8pdud1dpo72hf",
-        "host": "ec2-23-23-248-162.compute-1.amazonaws.com",
-        "port": "5432",
-        "dialect":"postgres",
-        "ssl": true,
-        "dialectOptions": {
-            "ssl": true
-        }});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+// const sequelize = new Sequelize({
+//         "username": "smbjtrlrzpewae",
+//         "password": "1e5a83e317065456cb66e3bcbc6fd46c8243f79f7bb09eb990ec844578746734",
+//         "database": "d8pdud1dpo72hf",
+//         "host": "ec2-23-23-248-162.compute-1.amazonaws.com",
+//         "port": "5432",
+//         "dialect":"postgres",
+//         "ssl": true,
+//         "dialectOptions": {
+//             "ssl": true
+//         }});
 
 const Lunch = sequelize.define('lunch', {
   team: Sequelize.STRING,
