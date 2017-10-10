cb9115fd89a52124f5bfbb2de6a6ed4c6ddf4ae9
Shem Magnezi <shem@Shems-MacBook-Pro.local>
2017-10-10 21:53:35 +0300

    fix

--- lunch_join.js
+++ lunch_join.js
@@ -13,14 +13,14 @@ module.exports = (message, slashCommand) => {
         finished: false,
       },
     }).then(lunches => {
-      if (!lunch) {
+      if (!lunches) {
         slashCommand.replyPrivate(message, 'no open lunch for this channel');
         return;
       }
 
       db.User.create({
         user: message.user_name,
-        lunch_id: lunch[0].id,
+        lunch_id: lunches[0].id,
       }).then(() => {
         slashCommand.replyPublic(message, `<@${message.user_name}> joined lunch!`);
       });
